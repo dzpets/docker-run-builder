bdef85f100b52f3c3890f0e54308c9b3ffb35149
Denis Zavershinskiy <zaverden@gmail.com>
2018-10-22 23:48:00 +0700

    add AppKeyValueList

---
+++ src/components/AppKeyValueList.vue
@@ -0,0 +1,75 @@
+<script>
+import Vue from "vue";
+const { floor, random } = Math;
+const getLast = arr => arr[arr.length - 1];
+
+export default {
+  props: {
+    items: Array,
+    buildNew: Function,
+    keyField: String,
+    valueField: String,
+    keyLabel: String,
+    valueLabel: String
+  },
+  data: function() {
+    return {
+      newItem: this.buildNew(),
+      idPrefix: {
+        key: `key${Date.now}${floor(1000*random())}`,
+        value: `value${Date.now}${floor(1000*random())}`,
+      }
+    };
+  },
+  methods: {
+    onNewChanged: function(side) {
+      this.$emit("add:item", this.newItem);
+      this.newItem = this.buildNew();
+      Vue.nextTick(() => getLast(this.$refs[`${side}Inputs`]).focus());
+    },
+    onChanged: function(item, index) {
+      if (!item[this.keyField] && !item[this.valueField]) {
+        this.$emit("remove:item", index);
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <div v-for="(item, index) of items" :key="index">
+      <label :for="`${idPrefix.key}${index}`">{{ keyLabel || keyField }}</label>
+      <input
+        ref="keyInputs"
+        type="text"
+        :id="`${idPrefix.key}${index}`"
+        v-model="item[keyField]"
+        @input="onChanged(item, index)">
+      <label :for="`${idPrefix.value}${index}`">{{ valueLabel || valueField }}</label>
+      <input
+        ref="valueInputs"
+        type="text"
+        :id="`${idPrefix.value}${index}`"
+        v-model="item[valueField]"
+        @input="onChanged(item, index)">
+    </div>
+    <div>
+      <label for="newItemKey">{{ keyLabel || keyField }}</label>
+      <input
+        type="text"
+        id="newItemKey"
+        v-model.number="newItem[keyField]"
+        @input="onNewChanged('key')">
+      <label for="newItemValue">{{ valueLabel || valueField }}</label>
+      <input
+        type="text"
+        id="newItemValue"
+        v-model.number="newItem[valueField]"
+        @input="onNewChanged('value')">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
